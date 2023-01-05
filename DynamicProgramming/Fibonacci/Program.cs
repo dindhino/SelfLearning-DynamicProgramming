ea198ef7c54bfa68b4b0c052a8643d1d8cc721a2
Dindin Dhino Alamsyah <dhino.alamsyah@geekseat.com.au>
2023-01-05 15:25:30 +0700

    Add Fibonacci

---
+++ DynamicProgramming/Fibonacci/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Fibo(int n)
+{
+    List<int> dp = new List<int>();
+    for (int i = 0; i <= n; i++)
+        dp.Add(-1);
+    return FiboHelper(n, dp);
+}
+
+int FiboHelper(int n, List<int> dp)
+{
+    if (n <= 1) return n;
+    if (dp[n] != -1) return dp[n];
+
+    return dp[n] = FiboHelper(n - 1, dp) + FiboHelper(n - 2, dp);
+}
+
+int n = 5;
+Console.WriteLine(Fibo(n));
+Console.ReadLine();
