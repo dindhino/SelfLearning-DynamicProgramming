e77bb10270074bcc3798e47e0b573dfbb8a9127c
Dindin Dhino Alamsyah <dhino.alamsyah@geekseat.com.au>
2023-01-10 15:24:36 +0700

    Add digit DP 2

---
+++ DynamicProgramming/DigitDP2/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int SumOfDigitsFrom1ToN(int n)
+{
+    if (n < 10)
+        return (n * (n + 1) / 2);
+
+    int d = (int)(Math.Log(n) / Math.Log(10));
+
+    int[] a = new int[d + 1];
+    a[0] = 0; a[1] = 45;
+
+    for (int i = 2; i <= d; i++)
+        a[i] = a[i - 1] * 10 + 45 *
+            (int)(Math.Ceiling(Math.Pow(10, i - 1)));
+
+    int p = (int)(Math.Ceiling(Math.Pow(10, d)));
+
+    int msd = n / p;
+
+    return (msd * a[d] + (msd * (msd - 1) / 2) * p +
+        msd * (1 + n % p) + SumOfDigitsFrom1ToN(n % p));
+}
+
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Sum of digits in numbers " +
+                    "from 1 to " + n + " is " + 
+                    SumOfDigitsFrom1ToN(n));
+
+Console.ReadLine();
