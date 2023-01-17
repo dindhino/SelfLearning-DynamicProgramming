fbd9d35fa7b803a2594368bc276c8d5702230c4d
Dindin Dhino Alamsyah <dhino.alamsyah@geekseat.com.au>
2023-01-17 16:36:04 +0700

    Add BinomialCoefficientSTE

---
+++ DynamicProgramming/BinomialCoefficientSTE/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+int BinomialCoeff(int n, int k)
+{
+    int res = 1;
+    if (k > n - k) k = n - k;
+
+    for (int i = 0; i < k; ++i)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+    return res;
+}
+
+int n = 5, k = 2;
+Console.Write("Value of C(" + n + "," + k + ") is "
+              + BinomialCoeff(n, k));
+
+Console.ReadLine();
