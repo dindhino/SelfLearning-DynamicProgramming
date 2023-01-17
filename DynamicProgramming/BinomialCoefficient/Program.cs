c3bcef533b92fd382025d5dc5381ab8e1cc5aa85
Dindin Dhino Alamsyah <dhino.alamsyah@geekseat.com.au>
2023-01-17 14:07:55 +0700

    Add BinomialCoefficient and BinomialCoefficientDP

---
+++ DynamicProgramming/BinomialCoefficient/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int BinomialCoeff(int n, int k)
+{
+    if (k > n) return 0;
+    if (k == 0 || k == n) return 1;
+    return BinomialCoeff(n - 1, k - 1)
+            + BinomialCoeff(n - 1, k);
+}
+
+int n = 5, k = 2;
+Console.Write("Value of C(" + n + "," + k + ") is "
+              + BinomialCoeff(n, k));
+
+Console.ReadLine();
---
+++ DynamicProgramming/BinomialCoefficientDP/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int BinomialCoeff(int n, int k)
+{
+    int[,] C = new int[n + 1, k + 1];
+    int i, j;
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= Math.Min(i, k); j++)
+        {
+            if (j == 0 || j == i)
+                C[i, j] = 1;
+            else
+                C[i, j] = C[i - 1, j - 1] + C[i - 1, j];
+        }
+    }
+
+    return C[n, k];
+}
+
+int n = 5, k = 2;
+Console.Write("Value of C(" + n + "," + k + ") is "
+              + BinomialCoeff(n, k));
+
+Console.ReadLine();
